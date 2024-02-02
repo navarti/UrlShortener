eec69aefd4bcc9df05989c7b8f70305600513810
Danylo-Pavlusenko <danil2005oleg@gmail.com>
2024-02-02 19:00:35 +0200

    Created asp.net project, added dtos based on entities, implemented automapping

---
+++ Shortner-Backend/Shortener.WebApi/DTOs/URLPairDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Shortener.WebApi.DTOs;
+
+public class URLPairDTO
+{
+    public Guid Id { get; set; }
+    public string LongUrl { get; set; } = "";
+    public string ShortUrl { get; set; } = "";
+    public DateTime CreationDate { get; set; }
+    public long ClickedPerMonth { get; set; }
+}
---
+++ Shortner-Backend/Shortener.WebApi/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using Shortener.Domain;
+using Shortener.WebApi.Util;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ShortenerDbContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
