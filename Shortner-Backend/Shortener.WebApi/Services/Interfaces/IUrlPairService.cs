6fc9fde7d0a6238c2a35030fdd861ba55e9fa8cd
Danylo-Pavlusenko <danil2005oleg@gmail.com>
2024-02-03 23:37:13 +0200

    Interface added

---
+++ Shortner-Backend/Shortener.WebApi/Services/Interfaces/IUrlPairService.cs
@@ -0,0 +1,12 @@
+﻿using Shortener.WebApi.DTOs;
+
+namespace Shortener.WebApi.Services.Interfaces;
+
+public interface IUrlPairService
+{
+    Task<UrlPairDTO> Create(UrlPairDTO urlPairCreateDTO);
+    Task<UrlPairDTO> Get(Guid id);
+    Task<IEnumerable<UrlPairDTO>> GetAll();
+    Task<UrlPairDTO> Update(UrlPairDTO urlPairCreateDTO);
+    Task Delete(Guid id);
+}
