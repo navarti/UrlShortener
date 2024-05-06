f22b18a4560a343bcd9a5d940c985b67f006611d
Danylo-Pavlusenko <danil2005oleg@gmail.com>
2024-05-06 20:22:57 +0300

    Front-end main sections developed

--- Shortener-Backend/Shortener.WebApi/Controllers/HomeController.cs
+++ Shortener-Backend/Shortener.WebApi/Controllers/HomeController.cs
@@ -13,12 +13,8 @@ namespace WebApplication1.Controllers
             _logger = logger;
         }
 
+        [HttpGet("/")]
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
---
+++ Shortener-Backend/Shortener.WebApi/wwwroot/js/UrlPair/Index.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $('#shortenBtn').click(function () {
+        var longUrl = $('#longUrl').val();
+        $.ajax({
+            type: 'POST',
+            url: '/UrlPair/Create',
+            contentType: 'application/json',
+            data: JSON.stringify({ longUrl: longUrl }),
+            success: function (response) {
+                $('#shortenedUrl').attr('href', 'https://top-url-shortener.azurewebsites.net/' + response.shortUrl);
+                $('#shortenedUrl').text('https://top-url-shortener.azurewebsites.net/' + response.shortUrl);
+                $('#shortenedUrlContainer').show();
+            },
+            error: function (xhr, status, error) {
+                if (xhr.status === 400) {
+                    alert(xhr.responseText); // Display the error message sent from the server
+                } else {
+                    alert('An error occurred while shortening URL.'); // Default error message
+                }
+            }
+        });
+    });
+});
